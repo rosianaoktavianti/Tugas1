cb06cb466981c3b04f4acc312adc25aaf3422c79
rosianaoktavianti <43130962+rosianaoktavianti@users.noreply.github.com>
2018-09-10 11:44:14 +0400

    Add files via upload

---
+++ tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author admin
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 4;
+        for (int i=1; i<=jumlah; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            }
+        }
+        
+    }
+    
+
